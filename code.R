660c85d3ff87570e3c3d6e443937ae4b196438c1
Anne Shi <AShi@pewresearch.org>
2018-07-02 12:41:00 -0400

    test

--- code.R
+++ code.R
@@ -2,3 +2,24 @@
 
 library(tidyverse)
 library(nycflights13)
+
+library(dslabs)
+data(heights)
+
+s <- heights %>% 
+  filter(sex == "Male") %>%
+  summarize(average = mean(height), standard_deviation = sd(height))
+
+s
+s$average
+s$standard_deviation
+
+heights %>% 
+  filter(sex == "Male") %>%
+  summarize(median = median(height), 
+            minimum = min(height),
+            maximum = max(height))
+
+
+
+
